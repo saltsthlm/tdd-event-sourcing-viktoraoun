c09027b3ae9f0d3837855ccdb62f4afc1db3f081
viktoraoun <150928018+viktoraoun@users.noreply.github.com>
2024-11-11 14:35:32 +0100

    Remove ClosureEvent_Should_CloseAccount

--- EventSourcing.Test/AccountAggregateTest.cs
+++ EventSourcing.Test/AccountAggregateTest.cs
@@ -367,27 +367,6 @@ public class AccountAggregateTEst
   }
 
   [Fact]
-  public async void ClosureEvent_Should_CloseAccount()
-  {
-    // Arrange
-    var events = await FileReader.GetStream(18);
-    var expectedAccount = new TestAccountAggregate
-    {
-      AccountId = "ACC123456",
-      Balance = 5000,
-      Currency = CurrencyType.Usd,
-      CustomerId = "CUST001",
-      Status = AccountStatus.Closed,
-    };
-
-    // Act
-    var result = AccountAggregate.GenerateAggregate(events);
-
-    // Assert
-    result.Should().BeEquivalentTo(expectedAccount);
-  }
-
-  [Fact]
   public async void ClosureEvent_Should_AddEventToAccountLog()
   {
     // Arrange
