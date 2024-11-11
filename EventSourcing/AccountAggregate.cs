2e2ee12d94320da90f6ab8d4abf1cc00448dfd9d
viktoraoun <150928018+viktoraoun@users.noreply.github.com>
2024-11-11 13:13:57 +0100

    Fix DepositEvent

--- EventSourcing/AccountAggregate.cs
+++ EventSourcing/AccountAggregate.cs
@@ -1,4 +1,4 @@
-﻿using EventSourcing.Events;
+using EventSourcing.Events;
 using EventSourcing.Exceptions;
 using EventSourcing.Models;
 
@@ -9,6 +9,7 @@ public class AccountAggregate
 
   public string? AccountId { get; set; }
   public decimal Balance { get; set; }
+  public decimal MaxBalance { get; set; }
   public CurrencyType Currency { get; set; }
   public string? CustomerId { get; set; }
   public AccountStatus Status { get; set; }
@@ -51,13 +52,24 @@ public class AccountAggregate
   {
     AccountId = accountCreated.AccountId;
     Balance = accountCreated.InitialBalance;
+    MaxBalance = accountCreated.MaxBalance;
     Currency = accountCreated.Currency;
     CustomerId = accountCreated.CustomerId;
   }
 
   private void Apply(DepositEvent deposit)
   {
+    if(AccountId == null)
+    {
+      throw new Exception("128 ERROR_ACCOUNT_UNINSTANTIATED");
+    }
+
     Balance += deposit.Amount;
+
+    if(Balance > MaxBalance)
+    {
+      throw new Exception("281 ERROR_BALANCE_SUCCEED_MAX_BALANCE");
+    }
   }
 
   private void Apply(WithdrawalEvent wihdrawal)
